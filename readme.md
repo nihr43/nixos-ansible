59de8f9ad66454cc5fd3584457efcc111d38134b
nathan hensel <nathanihensel@gmail.com>
2023-09-10 03:05:01 -0500

    check in poc

---
+++ readme.md
@@ -0,0 +1,3 @@
+# nixos-ansible
+
+This project presents a proof-of-concept for deploying declarative nixos configuration with ansible.
---
+++ roles/nixos/templates/configuration.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, ... }: {
+  imports = [
+    <nixpkgs/nixos/modules/profiles/all-hardware.nix>
+    <nixpkgs/nixos/modules/profiles/base.nix>
+    ./hardware-configuration.nix
+    ./ssh-keys.nix
+  ];
+
+  system.stateVersion = "23.11";
+  boot.loader.grub.devices = ["/dev/sda"];
+
+{% if serial is defined and serial == true %}
+  boot.kernelParams = [
+    "console=ttyS0,115200"
+  ];
+{% endif %}
+
+  nixpkgs.config.allowUnfree = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  networking.hostName = "{{hostname}}";
+  networking.nameservers = ["{{nameserver}}"];
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+    settings.PermitRootLogin = "yes";
+  };
+
+  users.mutableUsers = false;
+  users.users.root = {
+    # Password is "linux"
+    hashedPassword = lib.mkForce "$6$7IKExnDde920x.YH$ggegnnKJYdmg1Wt33fxuPpM.MmIaX32LXVyjL8ed7ohT385lKotFGzRpitncQ3pd9Lci1QCFGRn2tVJGxkFAm0";
+  };
+
+  environment.systemPackages = with pkgs; [
+    coreutils
+    htop
+    git
+    vim
+    python3
+  ];
+}
